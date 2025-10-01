b79d1bfae6491d233cee718cd7b472a8848caa63
Mehdi Guermazi <mehdi.guermazi@toumoro.com>
2025-10-01 00:36:35 +0000

    abstraction des services AiSearchService et AiChatbotService creation d'un service pour solr

---
+++ Classes/Service/SolrService.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace W3code\W3cAiconnector\Service;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Log\LoggerInterface;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
+
+class SolrService
+{
+    private const SOLR_ENDPOINT = 'http://solr:8983/solr/';
+
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function search(string $keywords, SiteLanguage $siteLanguage, array $filters = []): array
+    {
+        $solrCore = 'core_' . strtolower($siteLanguage->getHreflang());
+        $solrUrl = self::SOLR_ENDPOINT . $solrCore . '/select?q=' . urlencode($keywords);
+
+        if (!empty($filters)) {
+            foreach ($filters as $filter) {
+                $solrUrl .= '&fq=' . urlencode(stripslashes($filter));
+            }
+            $this->logger->info('Solr Filters', ['filters' => $filters]);
+        }
+
+        $client = new Client();
+        $results = [];
+        try {
+            $response = $client->get($solrUrl);
+            $solrData = json_decode((string)$response->getBody(), true);
+            $results = $solrData['response']['docs'] ?? [];
+            $variantIds = array_map(fn($result) => $result['variantId'] ?? null, $results);
+            $this->logger->info('Solr Results', ['variantIds' => $variantIds]);
+        } catch (GuzzleException $e) {
+            $this->logger->error('Solr Error', ['error' => $e->getMessage()]);
+        }
+
+        return $results;
+    }
+}
